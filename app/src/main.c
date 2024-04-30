bffa3803f7152a7c83cba93cb961351282ab5149
Mustafa Abdullah Kus <mustafa.kus@sparsetechnology.com>
2024-04-30 13:11:24 +0300

    app: src: main.c: add thread for prometheus metric exposition
    - create a structure for Prometheus context containing
    collector and counter.
    - implement a thread function 'prometheus_exposer_thread'
    to periodically increase metrics value.
    - define and start the thread 'prom_exposer_id'
    using K_THREAD_DEFINE.
    - adjust main function to use the new thread
    for Prometheus metric exposition.

--- app/src/main.c
+++ app/src/main.c
@@ -25,23 +25,42 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 #define PORT         8080
 #define METRICS_PATH "/metrics"
 
-int main(void)
+struct {
+
+	pm_collector_t *collector;
+
+	pm_counter_t *counter;
+
+} prom_context;
+
+static void prometheus_exposer_thread(void)
 {
 
+	while (true) {
+		LOG_INF(" >-- increase metrics value %ld", prom_context.counter->value);
+
+		pm_counter_inc(prom_context.counter);
+
+		k_sleep(K_SECONDS(1));
+	}
+}
+
+K_THREAD_DEFINE(prom_exposer_id, 2048, prometheus_exposer_thread, NULL, NULL, NULL, 5, 0, 2000);
+
+int main(void)
+{
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
 	// Create a mock collector with different types of metrics
-	pm_collector_t *collector = pm_collector_create();
+	prom_context.collector = pm_collector_create();
 
 	pm_label_t labels[] = {{"label1", "value1"}, {"label2", "value2"}};
-	pm_counter_t *counter = pm_counter_create("test_counter", "Test counter", labels, 2);
-	pm_counter_inc(counter);
+	prom_context.counter = pm_counter_create("test_counter", "Test counter", labels, 2);
+	pm_counter_inc(prom_context.counter);
 
-	pm_collector_register_metric(collector, (pm_metric_t *)counter);
+	pm_collector_register_metric(prom_context.collector, (pm_metric_t *)prom_context.counter);
 
-	// TO DO: Add more metrics to the collector and test them
-
-	pm_exposer_start(PORT, METRICS_PATH, collector);
+	pm_exposer_start(PORT, METRICS_PATH, prom_context.collector);
 
 	return 0;
 }
